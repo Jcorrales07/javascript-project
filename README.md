339b7b498fe0e8ee15258fead9e80290b7aa8da6
Joe Corrales <corralesjoe07@gmail.com>
2021-08-03 19:29:09 -0600

    Archivos de js y html, en js estan las formulas de las figuras geometricas. take it ease

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # javascript-project
+
+... 
+
+## Taller #1: figuras geometricas
+
+- 1er paso: definir las formulas 
+- 2do paso: implementar las formulas en JavaScript
+- 3er paso: crear funciones
+- 4to paso: integrar JS con HTML
---
+++ figuras.js
@@ -0,0 +1,26 @@
+const ladoCuadrado = 5; 
+console.log("lado del cuadrado miden: " + ladoCuadrado + "cm")
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadro es: " + perimetroCuadrado + "cm")
+
+const areaCuadrado = ladoCuadrado**2;
+console.log("El area del cuadro es: " + areaCuadrado + "cm" )
+
+// Codigo del triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4 ; 
+console.log
+(
+    "Los lados del triangulo miden: " 
+    + ladoTriangulo1 
+    + "cm, " 
+    + ladoTriangulo2 
+    + "cm, " 
+    + baseTriangulo 
+    + "cm"
+    )
+
+
