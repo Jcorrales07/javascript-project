d387c531edff17103f5a668c748751d7f7e84143
Joe Corrales <corralesjoe07@gmail.com>
2021-08-04 20:53:48 -0600

    Funciones para el calculo de las figuras, hice que se conectara con html

--- figuras.js
+++ figuras.js
@@ -1,26 +1,72 @@
-const ladoCuadrado = 5; 
-console.log("lado del cuadrado miden: " + ladoCuadrado + "cm")
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadro es: " + perimetroCuadrado + "cm")
+// Codigo del cuadrado
+console.group("Cuadrados")
 
-const areaCuadrado = ladoCuadrado**2;
-console.log("El area del cuadro es: " + areaCuadrado + "cm" )
+function perimetroCuadrado(lado) {
+    return lado * 4;
+} 
+
+perimetroCuadrado()
+
+function areaCuadrado(lado) {
+    return lado**2;
+} 
+
+console.groupEnd();
 
 // Codigo del triangulo
+console.group("Triangulos")
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4 ; 
-console.log
-(
-    "Los lados del triangulo miden: " 
-    + ladoTriangulo1 
-    + "cm, " 
-    + ladoTriangulo2 
-    + "cm, " 
-    + baseTriangulo 
-    + "cm"
-    )
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+};
 
 
+function areaTriangulo(base, altura) {
+    return (base * altura)/ 2;
+}
+
+console.groupEnd();
+
+// Codigo del Circulo 
+console.group("Circulos");
+
+// Radio
+const radioCirculo = 4;
+
+// Diametro
+function diametroCirculo(radio) {
+    return radio * 2;
+} 
+
+// PI
+const PI = Math.PI;
+
+// Circumferencia
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio)
+    return PI * diametro;
+}
+
+// Area
+function areaCirculo(radio) {
+    return PI * radio**2;
+}  
+console.groupEnd(); 
+
+// Aqui interacturamos con el HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado")
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro)
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado")
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area)
+}
